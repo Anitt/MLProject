6450390f4f8c2a6a1e0d07d9286f3e4693eca007
Anitt <an329828@dal.ca>
2024-07-31 11:09:25 -0400

    ProjectStructure &Logging & exception handling

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPHEN_E_DOT = "-e ."
+
 def get_requirements(file_path:str)->List[str]:
 
     '''
@@ -21,6 +22,8 @@ def get_requirements(file_path:str)->List[str]:
 
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
+
+        print(requirements)
 
     return requirements
 
